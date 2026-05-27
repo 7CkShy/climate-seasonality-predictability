2f8beb461faed0e3b98a6e9bc8b948314e02de21
7CkShy <ckshy@snnu.edu.cn>
2026-05-27 23:07:28 +0800

    add seasonality frequency caculation code

--- R/seasonality_calculation.R
+++ R/seasonality_calculation.R
@@ -1,3 +1,4 @@
+set.seed(6789)
 df <- data.frame(
   "year" = as.integer(rep(1990:2020, each = 12)),
   "month" = as.integer(rep(1:12, 31)),
@@ -12,3 +13,7 @@ df.monthly <- aggregate(Q ~ month + year, df, "mean", na.rm = TRUE)
 df.monthly$Q <- log10(df.monthly$Q + 1)
 df.monthly$class <- cut(df.monthly$Q, 10, right = FALSE, include.lowest = TRUE)
 df.table <- with(df.monthly, table(class, month))
+
+X <- apply(df.table, 2, sum, na.rm = T)
+Y <- apply(df.table, 1, sum, na.rm = T)
+Z <- sum(df.table, na.rm = T)
