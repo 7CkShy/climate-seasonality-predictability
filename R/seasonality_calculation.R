b48391070f55d1ae263303e90112944e010496ab
7CkShy-L <ckshy@snnu.edu.cn>
2026-05-28 09:58:03 +0800

    copy the seasonal caculation code of M

--- R/seasonality_calculation.R
+++ R/seasonality_calculation.R
@@ -1,3 +1,5 @@
+caculate_seasonal <- function() {}
+
 set.seed(6789)
 df <- data.frame(
   "year" = as.integer(rep(1990:2020, each = 12)),
@@ -14,6 +16,14 @@ df.monthly$Q <- log10(df.monthly$Q + 1)
 df.monthly$class <- cut(df.monthly$Q, 10, right = FALSE, include.lowest = TRUE)
 df.table <- with(df.monthly, table(class, month))
 
-X <- apply(df.table, 2, sum, na.rm = T)
-Y <- apply(df.table, 1, sum, na.rm = T)
-Z <- sum(df.table, na.rm = T)
+X <- apply(df.table, 2, sum, na.rm = TRUE)
+Y <- apply(df.table, 1, sum, na.rm = TRUE)
+Z <- sum(df.table, na.rm = TRUE)
+
+HX <- -1 * sum((X / Z) * log(X / Z, base = 2), na.rm = TRUE)
+HY <- -1 * sum((Y / Z) * log(Y / Z, base = 2), na.rm = TRUE)
+HXY <- -1 * sum((df.table / Z) * log(df.table / Z, base = 2), na.rm = TRUE)
+
+P <- round(1 - (HXY - HX) / log(10, base = 2), 2)
+C <- round(1 - HY / log(10, base = 2), 2)
+M <- round((HX + HY - HXY) / log(10, base = 2), 2)
