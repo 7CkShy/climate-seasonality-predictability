264b4bd0066105f68a3b7a1b621d3664f1e36498
7CkShy <ckshy@snnu.edu.cn>
2026-05-31 12:31:01 +0800

    Add tests for seasonality calculation with raster data extraction and visualization.

--- tests/test_seasonality_calculation.R
+++ tests/test_seasonality_calculation.R
@@ -25,3 +25,28 @@ seasonal_raster_c <- terra::app(
   output_type = "C",
   cores = 4
 )
+
+# 选取栅格中的一个点进行测试 -----
+library(sf)
+library(terra)
+library(tidyverse)
+
+t_p <- data.frame("id" = 1, "x" = 99, "y" = 38)
+t_p <- st_as_sf(t_p, coords = c("x", "y"), crs = 4326)
+
+tif_file <- list.files("./data/qilian_pre_1901_1920/", full.names = TRUE)
+r <- rast(tif_file)
+
+p_v <- extract(r, t_p)
+
+p_v <- p_v |>
+  pivot_longer(cols = starts_with("pre"), names_to = "time")
+
+p_v$index <- seq_along(p_v$value)
+
+ggplot(data = p_v, mapping = aes(x = index, y = value)) +
+  geom_point() +
+  geom_line() +
+  theme_bw()
+
+# result: 目前得出的结果是在早期的气候数据中，几乎每一年的月变化都是一样的，就会导致M计算的结果非常大，可能是由于早期数据不可直接获取的原因
